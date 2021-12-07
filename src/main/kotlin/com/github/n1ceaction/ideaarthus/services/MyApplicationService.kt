53a564b470301b775aea1fe325e8e82c59d2be11
GitHub Action <action@github.com>
2021-12-07 11:40:11 +0000

    Template cleanup

--- src/main/kotlin/com/github/n1ceaction/ideaarthus/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/n1ceaction/ideaarthus/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.n1ceaction.ideaarthus.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.n1ceaction.ideaarthus.MyBundle
 
 class MyApplicationService {
 
